5295f26b58bf79c812524fe12549631a2dd23c07
Harish S <harish.sq@gmail.com>
2025-01-08 21:47:14 +0530

    a

--- netlify/functions/send-mail.js
+++ netlify/functions/send-mail.js
@@ -1,15 +1,12 @@
 // netlify/functions/send-mail.js
 const sendMail = async (event) => {
-    // You can still handle logic like sending an email or similar here.
-    // For now, we will just return a success response.
-  
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Email sent successfully!"
-      }),
-    };
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Email sent successfully!"
+    }),
   };
-  
-  export { sendMail };
-  
+};
+
+// Use ES module export
+export { sendMail as handler };
