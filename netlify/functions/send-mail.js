e963430e74b3751d9cc72fcf994b7c5552ed8c7f
Harish S <harish.sq@gmail.com>
2025-01-09 20:13:17 +0530

    as

--- netlify/functions/send-mail.js
+++ netlify/functions/send-mail.js
@@ -39,7 +39,7 @@ const sendEmailWithAttachment = async (
 
   try {
     await sgMail.send(msg);
-    console.log("Email sent successfully!");
+    console.log("Email sent successfully!!!!!!");
   } catch (error) {
     console.error("Error sending email:", error);
   }
